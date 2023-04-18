e192cbd3d34ae2b726364a49b969b33abb821d0c
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-18 12:27:35 +0100

    added base header with nav function

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,3 +35,50 @@ const links = [
       anchor: "login",
     },
   ];
+
+const Header = () => {
+    return (
+        <Box
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            translateY={0}
+            transitionProperty="transform"
+            transitionDuration=".3s"
+            transitionTimingFunction="ease-in-out"
+            backgroundColor="whiteAlpha.50"
+        >
+            <Box 
+            // color="gray.500" 
+            margin="0 auto"
+            >
+                <HStack
+                    px={16}
+                    py={4}
+                    justifyContent="space-between"
+                    alignItems="center"
+                    marginLeft={"60px"}
+                    marginRight={"60px"}
+                >
+                    <nav>
+                        <HStack spacing={8}>
+                            <Image src={logo} alt="Logo" height={"60px"} />
+                        </HStack>
+                    </nav>
+                    <nav>
+                        <HStack spacing={8}>
+                            {links.map(link => 
+                            <Box rounded={"md"} padding={"5px"} bgColor={"#495E57"}>
+                                <a key={link.anchor} href={link.url}> {link.name}</a>
+                            </Box>)
+                            }
+                        </HStack>
+                    </nav>
+                </HStack>
+            </Box>
+        </Box>
+    );
+};
+
+export default Header;
