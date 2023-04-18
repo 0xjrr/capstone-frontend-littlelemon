6f282cadb106cbd2b088051ffb73bd5dd9587dea
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-18 12:25:22 +0100

    added semantic tags, commented unimplemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import Header from './components/Header'
+// import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <h1>!</h1>
-    </div>
+    <>
+      <ChakraProvider>
+        <Header />
+        {/* <LandingSection />
+      <Footer /> */}
+      </ChakraProvider>
+    </>
   );
 }
 
