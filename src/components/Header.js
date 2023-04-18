2daa0f6af575811eb080442e0568be8ad6277911
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-18 12:27:07 +0100

    added links list and imports

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import { Box, HStack, Image } from "@chakra-ui/react";
+import logo from "../images/Asset 16@4x.png"
+
+const links = [
+    {
+      name: "Home",
+      url: "/#home",
+      anchor: "home",
+    },
+    {
+      name: "About",
+      url: "/#about",
+      anchor: "about",
+    },
+    {
+      name: "Menu",
+      url: "/#menu",
+      anchor: "menu",
+    },
+    {
+      name: "Reservations",
+      url: "/#reserve",
+      anchor: "reserve",
+    },
+    {
+      name: "Order online",
+      url: "/#order",
+      anchor: "order",
+    },
+    {
+      name: "Login",
+      url: "/#login",
+      anchor: "login",
+    },
+  ];
