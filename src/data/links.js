3a870a86a7c7cae4c225b22d0f431967f48c44d1
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-18 18:01:43 +0100

    add links constant variable

---
+++ src/data/links.js
@@ -0,0 +1,34 @@
+const links = [
+    {
+        name: "Home",
+        url: "/#home",
+        anchor: "home",
+    },
+    {
+        name: "About",
+        url: "/#about",
+        anchor: "about",
+    },
+    {
+        name: "Menu",
+        url: "/#menu",
+        anchor: "menu",
+    },
+    {
+        name: "Reservations",
+        url: "/#reserve",
+        anchor: "reserve",
+    },
+    {
+        name: "Order online",
+        url: "/#order",
+        anchor: "order",
+    },
+    {
+        name: "Login",
+        url: "/#login",
+        anchor: "login",
+    },
+];
+
+export default links;
