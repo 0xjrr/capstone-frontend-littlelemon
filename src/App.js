b25e8092b30026fc5b08f7113ea54b9df4b23349
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-18 15:26:28 +0100

    uncomment footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import Header from './components/Header'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <ChakraProvider>
         <Header />
-        {/* <LandingSection />
-      <Footer /> */}
+        {/*<LandingSection />*/}
+        <Footer />
       </ChakraProvider>
     </>
   );
