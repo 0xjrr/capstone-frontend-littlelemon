2d22c75ef0bd14d3ef672096d8ad29dee9a2a547
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-19 17:37:26 +0100

    SpecialsSection base

---
+++ src/components/SpecialsSection.js
@@ -0,0 +1,8 @@
+import { Text, Box, HStack, VStack, Image, Spacer } from "@chakra-ui/react";
+
+const SpecialsSection = () => {
+    return(
+        <Box height={"200px"}></Box>
+    );
+};
+export default SpecialsSection;
