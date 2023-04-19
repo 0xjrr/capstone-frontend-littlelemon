d382bae8b12971340720d320a542f39ad4dc95ab
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-19 22:46:27 +0100

    add specials first row elements

--- src/components/SpecialsSection.js
+++ src/components/SpecialsSection.js
@@ -1,8 +1,25 @@
-import { Text, Box, HStack, VStack, Image, Spacer } from "@chakra-ui/react";
+import { Text, Box, HStack, VStack, Image, Spacer, Flex } from "@chakra-ui/react";
+import ButtonYellow from "./ButtonYellow";
 
 const SpecialsSection = () => {
-    return(
-        <Box height={"200px"}></Box>
+    return (
+        <Box height={"984px"} margin={"80px 188px"}>
+            <VStack>
+                <Flex width={"100%"}>
+                    <Box>
+                        <Text fontSize={"18pt"}>
+                            Specials
+                        </Text>
+                    </Box>
+                    <Spacer />
+                    <Box>
+                        <ButtonYellow buttonText={"Menu"} urlEndpoint={"menu"} />
+                    </Box>
+                    <Spacer />
+                </Flex>
+
+            </VStack>
+        </Box>
     );
 };
 export default SpecialsSection;
