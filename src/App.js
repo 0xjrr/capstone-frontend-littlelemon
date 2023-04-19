823d9f4816ec1d61a4bf5c53b02657d444abc309
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-19 01:23:24 +0100

    add router components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomePage from './components/HomePage';
+import ReservePage from './components/ReservePage';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from  '@chakra-ui/react'
 
 function App() {
   return (
     <>
       <ChakraProvider>
         <Header />
-        {/*<LandingSection />*/}
+        <Box height={"124px"} />
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/booking" element={<ReservePage />}></Route>
+        </Routes>
+        </BrowserRouter>
         <Footer />
       </ChakraProvider>
     </>
