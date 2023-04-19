cd3995e25ae8a163e9890bc5f5bcf868f1e02ff9
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-19 01:35:11 +0100

    replace with location component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,11 @@
+
 import { Text, Box, HStack, VStack, Image } from "@chakra-ui/react";
+import Location from "./Location";
 const HeroSection = () => {
     return (
         <Box bgColor={"#495E57"} height={"512px"} >
             <HStack margin={"0 188px"}>
-                <VStack>
-                    <Text fontSize={"53px"}>
-                        Little Lemon
-                    </Text>
-                </VStack>
+                <Location />
             </HStack>
 
         </Box>
