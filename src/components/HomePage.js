6ef8332accf867d5cca6cd90a28f18a4430103e8
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-19 17:37:05 +0100

    homepage update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,11 @@
 import HeroSection from "./HeroSection";
+import SpecialsSection from "./SpecialsSection";
 
 const HomePage = () => {
     return(
         <div>
             <HeroSection />
+            <SpecialsSection />
 
         </div>
     );
