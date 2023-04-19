8ea4b741587e0d63e11d3c84d541ba06ba565df2
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-19 01:34:54 +0100

    location component creation

---
+++ src/components/Location.js
@@ -0,0 +1,15 @@
+import { Text, VStack} from "@chakra-ui/react";
+
+const Location = () => {
+    return (
+    <VStack align={"right"}>
+        <Text fontSize={"40pt"}>
+            Little Lemon
+        </Text>
+        <Text fontSize={"18pt"}>
+            Chicago
+        </Text>
+    </VStack>
+    );
+};
+export default Location;
