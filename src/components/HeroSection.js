22f23e68380c1ca04ce6b390b8141afcffaf9471
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-19 01:53:04 +0100

    implemented hero section components

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,18 @@
 
 import { Text, Box, HStack, VStack, Image } from "@chakra-ui/react";
 import Location from "./Location";
+import ButtonYellow from "./ButtonYellow";
 const HeroSection = () => {
     return (
         <Box bgColor={"#495E57"} height={"512px"} >
             <HStack margin={"0 188px"}>
-                <Location />
+                <VStack align={"right"}>
+                    <Location />
+                    <Text width={"300px"}>
+                    We are a family owned, Mediterranean restaurant, focused on traditional recipes with a modern twist.
+                    </Text>
+                    <ButtonYellow buttonText={"Reserve"} urlEndpoint={"reserve"}/>
+                </VStack>
             </HStack>
 
         </Box>
