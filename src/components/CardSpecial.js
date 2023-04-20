2425ecac0a435a11684268db119e9f2bf65a0a6e
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-20 16:28:13 +0100

    change font weight add order button alignment

--- src/components/CardSpecial.js
+++ src/components/CardSpecial.js
@@ -1,26 +1,31 @@
-import { Text, Box, VStack, Image, Spacer, Flex } from "@chakra-ui/react";
+import { Text, Box, VStack, Image, Spacer, Flex, HStack } from "@chakra-ui/react";
 
 const CardSpecial = (props) => {
-    return(
-        <Box height={"660px"} width={"306px"} bgColor={"#D0D0D0"} rounded={"md"}>
-            <VStack>
-                <Image src={props.itemImage} rounded={"md"}/> 
+    return (
+        <Box height={"660px"} width={"306px"} minWidth={"306px"} bgColor={"#D0D0D0"} rounded={"md"}>
+            <VStack height={"100%"}>
+                <Image src={props.itemImage} rounded={"md"} fit={"cover"} height={"200px"} width={"100%"} />
                 <Flex width={"80%"}>
-                    <Text>
+                    <Text fontWeight={"bold"}>
                         {props.itemName}
                     </Text>
                     <Spacer />
-                    <Text color={"#EE9972"}>
+                    <Text color={"#EE9972"} fontWeight={"bold"}>
                         ${props.itemPrice}
                     </Text>
                 </Flex>
-                <Text>
-                    {props.itemDescription}
-                </Text>
-                <Box>
-                    <Text>
-                        <a href="/order">Order</a>
+                <Box width={"80%"}>
+
+                    <Text align={"left"}>
+                        {props.itemDescription}
                     </Text>
+                </Box>
+                <Spacer />
+                <Box position={"relative"} bottom={"10px"} width={"80%"}>
+                    <HStack color="#495E57" fontWeight={"bold"}>
+                        <Spacer />
+                        <a href="/order" align="left">Order</a>
+                    </HStack>
                 </Box>
             </VStack>
 
