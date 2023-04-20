e12478f44209491a4f5e28c971a0677a9077acd7
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-20 16:26:45 +0100

    update menuItems image and description

--- src/data/menuItems.js
+++ src/data/menuItems.js
@@ -4,44 +4,44 @@ const menuItems = [
     {
       "name": "Mezze Platter",
       "price": 12,
-      "description": "A delicious assortment of hummus, baba ghanoush, tzatziki, olives, and pita bread. Perfect for sharing!",
-      "image": "restaurant.jpg"
+      "description": "A delightful array of flavors, this Mezze platter is the perfect way to start your meal. Savor the creamy hummus, the smoky baba ghanoush, the refreshing tzatziki, and the tangy olives with every bite. Dip the warm, fluffy pita bread into each dip and enjoy the contrasting textures and flavors. A great dish for sharing with friends or family!",
+      "image": "mezze.jpg"
     },
     {
       "name": "Greek Salad",
       "price": 10,
-      "description": "Fresh greens, juicy tomatoes, tangy feta cheese, and kalamata olives, tossed in a light vinaigrette.",
-      "image": "restaurant.jpg"
+      "description": "This Greek salad is a refreshing mix of crisp greens, ripe tomatoes, crumbly feta cheese, and flavorful kalamata olives, tossed in a light vinaigrette. Every bite offers a burst of Mediterranean flavors that will tantalize your taste buds. Perfect as a side or a light meal on its own.",
+      "image": "greeksalad.jpg"
     },
     {
       "name": "Falafel Wrap",
       "price": 8,
-      "description": "Crispy falafel balls, wrapped in warm pita bread, with lettuce, tomato, cucumber, and a drizzle of tahini sauce.",
-      "image": "restaurant.jpg"
+      "description": "Our crispy falafel wrap is a must-try! Enjoy the savory falafel balls wrapped in warm pita bread, along with fresh lettuce, juicy tomato, cool cucumber, and a generous drizzle of tahini sauce. The combination of textures and flavors is simply divine. Vegetarian-friendly and packed with protein, this wrap is a great choice for any meal of the day.",
+      "image": "falafel.jpg"
     },
     {
       "name": "Chicken Shawarma",
       "price": 14,
-      "description": "Marinated chicken, slow-cooked and thinly sliced, served in a warm pita wrap with garlic sauce, lettuce, tomato, and pickles.",
-      "image": "restaurant.jpg"
+      "description": "This Middle Eastern classic is a flavorful dish you won't want to miss. Tender, marinated chicken, slow-cooked to perfection, is thinly sliced and wrapped in warm pita bread. Topped with a garlic sauce, lettuce, tomato, and pickles, this sandwich offers a delicious mix of spicy and tangy flavors that will leave you wanting more. A hearty meal that is perfect for lunch or dinner.",
+      "image": "shawarma.jpg"
     },
     {
       "name": "Seafood Paella",
       "price": 20,
-      "description": "A flavorful Spanish rice dish, loaded with shrimp, mussels, clams, and calamari, cooked to perfection with saffron and other Mediterranean spices.",
-      "image": "restaurant.jpg"
+      "description": "Our Seafood Paella is a Spanish classic that will transport you straight to the Mediterranean. A colorful and aromatic rice dish, it features succulent shrimp, tender mussels, briny clams, and savory calamari, cooked with saffron and other Mediterranean spices. Every spoonful is packed with flavor and texture that will take your taste buds on a journey. A perfect dish for seafood lovers and anyone looking for a flavorful meal.",
+      "image": "paella.jpg"
     },
     {
       "name": "Baklava",
       "price": 6,
-      "description": "A sweet and nutty pastry, made with layers of crispy phyllo dough and a honey syrup, topped with crushed pistachios.",
-      "image": "restaurant.jpg"
+      "description": "A sweet and nutty pastry that is a true Mediterranean delicacy. Made with layers of crispy phyllo dough, crunchy pistachios, and sweet honey syrup, this dessert is the perfect end to your meal. The combination of textures and flavors is truly a work of art that will leave you wanting more.",
+      "image": "baklava.jpg"
     },
     {
       "name": "Turkish Coffee",
       "price": 4,
-      "description": "A strong, rich coffee, brewed in a traditional copper pot and served with a piece of Turkish delight on the side.",
-      "image": "restaurant.jpg"
+      "description": "Experience the rich and bold flavors of traditional Turkish coffee. Brewed in a copper pot and served piping hot, this coffee is strong and flavorful, with a unique texture and aroma. Sip it slowly and savor every drop, and enjoy the complementary Turkish delight on the side. A perfect end to any meal.",
+      "image": "turkishcoffee.jpg"
     }
   ];
 
