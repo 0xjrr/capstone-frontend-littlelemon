3694608e17e129724d14c109ed52b81dcd912c2f
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-20 01:05:01 +0100

    menuitems text and placehorder images

---
+++ src/data/menuItems.js
@@ -0,0 +1,48 @@
+import mezzeplatter from "../images/restaurant.jpg"
+
+const menuItems = [
+    {
+      "name": "Mezze Platter",
+      "price": 12,
+      "description": "A delicious assortment of hummus, baba ghanoush, tzatziki, olives, and pita bread. Perfect for sharing!",
+      "image": "restaurant.jpg"
+    },
+    {
+      "name": "Greek Salad",
+      "price": 10,
+      "description": "Fresh greens, juicy tomatoes, tangy feta cheese, and kalamata olives, tossed in a light vinaigrette.",
+      "image": "restaurant.jpg"
+    },
+    {
+      "name": "Falafel Wrap",
+      "price": 8,
+      "description": "Crispy falafel balls, wrapped in warm pita bread, with lettuce, tomato, cucumber, and a drizzle of tahini sauce.",
+      "image": "restaurant.jpg"
+    },
+    {
+      "name": "Chicken Shawarma",
+      "price": 14,
+      "description": "Marinated chicken, slow-cooked and thinly sliced, served in a warm pita wrap with garlic sauce, lettuce, tomato, and pickles.",
+      "image": "restaurant.jpg"
+    },
+    {
+      "name": "Seafood Paella",
+      "price": 20,
+      "description": "A flavorful Spanish rice dish, loaded with shrimp, mussels, clams, and calamari, cooked to perfection with saffron and other Mediterranean spices.",
+      "image": "restaurant.jpg"
+    },
+    {
+      "name": "Baklava",
+      "price": 6,
+      "description": "A sweet and nutty pastry, made with layers of crispy phyllo dough and a honey syrup, topped with crushed pistachios.",
+      "image": "restaurant.jpg"
+    },
+    {
+      "name": "Turkish Coffee",
+      "price": 4,
+      "description": "A strong, rich coffee, brewed in a traditional copper pot and served with a piece of Turkish delight on the side.",
+      "image": "restaurant.jpg"
+    }
+  ];
+
+export default menuItems;
