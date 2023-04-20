19bd2e08098d535aa5355df2628c06cd828a4142
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-20 12:56:27 +0100

    small fixes on specials

--- src/components/SpecialsSection.js
+++ src/components/SpecialsSection.js
@@ -1,4 +1,4 @@
-import { Text, Box, HStack, VStack, Image, Spacer, Flex } from "@chakra-ui/react";
+import { Text, Box, HStack, VStack, Image, Spacer, Flex, Center } from "@chakra-ui/react";
 import ButtonYellow from "./ButtonYellow";
 import CardSpecial from "./CardSpecial";
 import menuItems from "../data/menuItems";
@@ -8,19 +8,22 @@ const SpecialsSection = () => {
         <Box height={"984px"} margin={"80px 188px"}>
             <VStack>
                 <Flex width={"100%"}>
-                    <Box>
+                    <Box width={"300px"}>
                         <Text fontSize={"18pt"}>
                             Specials
                         </Text>
                     </Box>
                     <Spacer />
-                    <Box>
-                        <ButtonYellow buttonText={"Menu"} urlEndpoint={"menu"} />
+                    <Box width={"297px"}>
+                        <Center>
+                            <ButtonYellow buttonText={"Menu"} urlEndpoint={"menu"} />
+                        </Center>
                     </Box>
                     <Spacer />
                 </Flex>
+                <Spacer />
                 <HStack>
-                    {menuItems.map(item => <CardSpecial itemName={item.name} itemPrice={item.price} itemDescription={item.description} itemImage={item.image}/>)}
+                    {menuItems.map(item => <CardSpecial itemName={item.name} itemPrice={item.price} itemDescription={item.description} itemImage={item.image} />)}
                 </HStack>
             </VStack>
         </Box>
