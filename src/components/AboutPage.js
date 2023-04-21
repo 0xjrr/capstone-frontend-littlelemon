12eef649c865d3d2f1196413d99f78ccd6c85295
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-21 20:25:26 +0100

    AboutPage create

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import HeroSection from "./HeroSection";
+import AboutSectionLarge from "./AboutSectionLarge";
+
+
+
+const AboutPage = () => {
+    return(
+        <div>
+            <HeroSection />
+            <AboutSectionLarge />
+        </div>
+    );
+};
+
+export default AboutPage;
