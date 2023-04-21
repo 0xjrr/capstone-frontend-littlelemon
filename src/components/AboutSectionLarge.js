b4289d80eb0e4d1a10608e160028770deee3ae23
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-21 23:30:12 +0100

    fix about page

--- src/components/AboutSectionLarge.js
+++ src/components/AboutSectionLarge.js
@@ -1,4 +1,4 @@
-import { Text, Box, VStack, Spacer, Flex, Center } from "@chakra-ui/react";
+import { Text, Box, VStack, Spacer, Flex, Center, HStack } from "@chakra-ui/react";
 import ButtonYellow from "./ButtonYellow";
 
 const SpecialsSection = () => {
@@ -19,14 +19,17 @@ const SpecialsSection = () => {
                     </Box>
                 </Flex>
                 <Spacer />
-
-                <Text>
-                    Welcome to Little Lemon, a charming Mediterranean restaurant that will transport you to the sunny shores of the Mediterranean. Our restaurant is dedicated to bringing you the freshest and most authentic Mediterranean cuisine possible. Our dishes are crafted with the finest ingredients, expertly seasoned with a blend of herbs and spices that will tantalize your taste buds.
-
-                    Our cozy and intimate restaurant is perfect for a romantic date or a small gathering with friends and family. We take pride in our warm and friendly service, ensuring that every guest feels at home when dining with us. Whether you're craving our savory falafel wrap, our delicious chicken shawarma, or our mouthwatering seafood paella, we guarantee that you'll be impressed with our delectable menu offerings.
-
-                    At Little Lemon, we believe that good food should be enjoyed by everyone, and that's why we offer vegetarian and gluten-free options to cater to all dietary needs. Come dine with us and experience the flavors of the Mediterranean, right here in the heart of the city.
-                </Text>
+                <HStack>
+                    <Box width={"50%"}>
+                        <Text align={"justify"}>
+                            Welcome to Little Lemon, a charming Mediterranean restaurant that will transport you to the sunny shores of the Mediterranean. Our restaurant is dedicated to bringing you the freshest and most authentic Mediterranean cuisine possible. Our dishes are crafted with the finest ingredients, expertly seasoned with a blend of herbs and spices that will tantalize your taste buds.
+                            <br></br>
+                            Our cozy and intimate restaurant is perfect for a romantic date or a small gathering with friends and family. We take pride in our warm and friendly service, ensuring that every guest feels at home when dining with us. Whether you're craving our savory falafel wrap, our delicious chicken shawarma, or our mouthwatering seafood paella, we guarantee that you'll be impressed with our delectable menu offerings.
+                            <br></br>
+                            At Little Lemon, we believe that good food should be enjoyed by everyone, and that's why we offer vegetarian and gluten-free options to cater to all dietary needs. Come dine with us and experience the flavors of the Mediterranean, right here in the heart of the city.
+                        </Text>
+                    </Box>
+                </HStack>
             </VStack>
         </Box>
     );
