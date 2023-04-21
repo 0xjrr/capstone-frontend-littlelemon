90546ed142bc780f644beb783c3f58cb5e616703
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-21 17:12:54 +0100

    add a specials list from menu list

---
+++ src/data/specialsItems.js
@@ -0,0 +1,30 @@
+import mezzeplatter from "../images/restaurant.jpg"
+
+const menuItems = [
+    {
+      "name": "Mezze Platter",
+      "price": 12,
+      "description": "A delightful array of flavors, this Mezze platter is the perfect way to start your meal. Savor the creamy hummus, the smoky baba ghanoush, the refreshing tzatziki, and the tangy olives with every bite. Dip the warm, fluffy pita bread into each dip and enjoy the contrasting textures and flavors. A great dish for sharing with friends or family!",
+      "image": "mezze.jpg"
+    },
+    {
+      "name": "Greek Salad",
+      "price": 10,
+      "description": "This Greek salad is a refreshing mix of crisp greens, ripe tomatoes, crumbly feta cheese, and flavorful kalamata olives, tossed in a light vinaigrette. Every bite offers a burst of Mediterranean flavors that will tantalize your taste buds. Perfect as a side or a light meal on its own.",
+      "image": "greeksalad.jpg"
+    },
+    {
+      "name": "Falafel Wrap",
+      "price": 8,
+      "description": "Our crispy falafel wrap is a must-try! Enjoy the savory falafel balls wrapped in warm pita bread, along with fresh lettuce, juicy tomato, cool cucumber, and a generous drizzle of tahini sauce. The combination of textures and flavors is simply divine. Vegetarian-friendly and packed with protein, this wrap is a great choice for any meal of the day.",
+      "image": "falafel.jpg"
+    },
+    {
+      "name": "Chicken Shawarma",
+      "price": 14,
+      "description": "This Middle Eastern classic is a flavorful dish you won't want to miss. Tender, marinated chicken, slow-cooked to perfection, is thinly sliced and wrapped in warm pita bread. Topped with a garlic sauce, lettuce, tomato, and pickles, this sandwich offers a delicious mix of spicy and tangy flavors that will leave you wanting more. A hearty meal that is perfect for lunch or dinner.",
+      "image": "shawarma.jpg"
+    },
+  ];
+
+export default menuItems;
