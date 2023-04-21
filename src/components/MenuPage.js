96e8545ac1ec50ea6a1a5cabec5c63916019282d
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-21 23:29:56 +0100

    add menu page

---
+++ src/components/MenuPage.js
@@ -0,0 +1,15 @@
+import HeroSection from "./HeroSection";
+import MenuSection from "./MenuSection";
+
+
+
+const AboutPage = () => {
+    return(
+        <div>
+            <HeroSection />
+            <MenuSection />
+        </div>
+    );
+};
+
+export default AboutPage;
