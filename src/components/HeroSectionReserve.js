0b4af7ebccb170aedc03322588fcf2e43e9474d0
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-22 11:04:03 +0100

    add hero section for reserve page

---
+++ src/components/HeroSectionReserve.js
@@ -0,0 +1,29 @@
+import { Text, Box, HStack, VStack, Image, Spacer } from "@chakra-ui/react";
+import Location from "./Location";
+import heroSectionImage from "../images/herosection.jpg"
+
+const HeroSection = () => {
+    return (
+        <Box bgColor={"#495E57"} height={"512px"} position={"relative"}>
+            <HStack margin={"0 188px"}>
+                <Box position={"absolute"} top={"15%"}>
+
+                <VStack align={"right"} >
+                    <Location color={"white"}/>
+                    <Text width={"300px"} color={"white"}>
+                        We are a family owned, Mediterranean restaurant, focused on traditional recipes with a modern twist.
+                    </Text>
+                    <Spacer />
+                </VStack>
+                </Box>
+                <Spacer />
+                <Box paddingRight={"15%"}>
+                    <Image marginTop={"15%"} src={heroSectionImage} width={"450px"} height={"491px"} rounded={"md"} shadow={"base"} fit={"cover"}></Image>
+                </Box>
+                
+            </HStack>
+
+        </Box>
+    )
+};
+export default HeroSection;
