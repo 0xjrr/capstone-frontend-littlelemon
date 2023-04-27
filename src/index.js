79c2221a9783edd944b08b04939e9e93515c1db4
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-04-27 19:53:13 +0100

    add basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter basename="/">
+  <HashRouter basename="/capstone-frontend-littlelemon">
     <React.StrictMode>
       <App />
     </React.StrictMode>
